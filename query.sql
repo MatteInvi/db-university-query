21d8a500e981e0e971ca98c735b581915a77533f
MatteInvi <matteoinvidia@gmail.com>
2025-03-23 10:22:29 +0100

    feat: fix punto 3

--- query.sql
+++ query.sql
@@ -9,7 +9,9 @@ WHERE (`cfu`) > 10;
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
 SELECT *
 FROM `students`
-WHERE YEAR(CURRENT_DATE) - YEAR(`date_of_birth`) > "30" ;
+WHERE YEAR(CURRENT_DATE) - YEAR(`date_of_birth`) > "30"
+AND MONTH(CURRENT_DATE) >= MONTH(`date_of_birth`)
+AND DAY(CURRENT_DATE) >= DAY(`date_of_birth`) ;
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di
 -- laurea (286)
 SELECT *
